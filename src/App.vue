b433dce24d0813c2f4e25d65ec61df6657cf2ab0
omar-nav <omar.navarro@g.austincc.edu>
2018-12-27 11:43:29 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <appHeader></appHeader>
+    <hr>
+    <div class="row">
+      <servers></servers>
+      <appServerDetails></appServerDetails>
+    </div>
+    <hr>
+    <appFooter></appFooter>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import Servers from "./components/Server/Servers.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
+
+export default {
+  components: {
+    appHeader: Header,
+    Servers,
+    appServerDetails: ServerDetails,
+    appFooter: Footer
+  }
+};
+</script>
+
+<style>
+</style>
